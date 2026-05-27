6730aa7d66acb4e7602ae55e2906052e720c0b25
ating0912 <162403224+ating0912@users.noreply.github.com>
2026-05-27 11:06:21 +0800

    Update ecmade_moo_hci_platform_v8.py

--- ecmade_moo_hci_platform_v8.py
+++ ecmade_moo_hci_platform_v8.py
@@ -125,6 +125,35 @@ def choose_ecmade_record(records):
     pool = ecmade if ecmade else records
     return sorted(pool, key=lambda r: (abs(r["K"] - 10), r["seed"]))[0]
 
+def append_google_sheet(path,row):
+
+    scope = [
+
+        "https://www.googleapis.com/auth/spreadsheets",
+
+        "https://www.googleapis.com/auth/drive"
+
+    ]
+
+    creds=Credentials.from_service_account_info(
+
+        st.secrets["gcp_service_account"],
+
+        scopes=scope
+
+    )
+
+    client=gspread.authorize(creds)
+
+    sheet=client.open(
+        "ECMADE_HCI_Log"
+    )
+
+    worksheet=sheet.sheet1
+
+    worksheet.append_row(
+        list(row.values())
+    )
 
 def append_csv(path: Path, row: Dict):
     """
